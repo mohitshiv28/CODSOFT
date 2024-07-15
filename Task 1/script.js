2d1b0b528a853c35d98beb254a2f2e30239bd279
Mohit <mohitshivhare486@gmail.com>
2024-07-15 21:36:12 +0530

    first commit

---
+++ Task 1/script.js
@@ -0,0 +1,6 @@
+let menu=document.querySelector('#menu-icon');
+let navlinks=document.querySelector('.navlinks');
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlinks.classList.toggle('open');
+}
